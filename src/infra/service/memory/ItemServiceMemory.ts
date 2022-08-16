bfcd97391564221bdd5f0164eb76b3abd1768fa5
Filipe Soares <filipecarlossoares@gmail.com>
2022-08-16 20:56:04 -0300

    place order

---
+++ src/infra/service/memory/ItemServiceMemory.ts
@@ -0,0 +1,12 @@
+import Item from "../../../entities/Item";
+
+export default class ItemServiceMemory {
+
+	async list () {
+		const items: Item[] = [];
+		items.push(new Item(4, "Bateria", 7000));
+		items.push(new Item(5, "Violão", 200));
+		items.push(new Item(6, "Banjo", 1000));
+		return items;
+	}
+}
